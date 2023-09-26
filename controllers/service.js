61c08bcd4651783475db4158a3bf84653aa1cbaf
codebyps <priyanshu.creator.dev@gmail.com>
2023-09-26 20:33:58 +0530

    new replcations

--- controllers/service.js
+++ controllers/service.js
@@ -1,39 +1,33 @@
 import fetch from "node-fetch";
 
-import { convertInstagramReelsToDdInstagram } from "../utils.js";
+import {
+  convertInstagramReelsToDdInstagram,
+  getElementFromHTML,
+} from "../utils.js";
 
 export async function getReels(req, res) {
   let id = 0;
 
   try {
+    id = id + 1;
     const apiKey = "priyanshu";
 
     const urlParam = req.params[0];
     const convertedUrl = convertInstagramReelsToDdInstagram(urlParam);
 
-    if (!apiKey) {
-      res.status(401).json({
-        message: "API key is required.",
-        error: "Unauthorized",
-      });
-    }
-
-    if (apiKey != "priyanshu") {
-      res.status(401).json({
-        message: "API key is invalid.",
-        error: "Unauthorized",
-      });
-    }
-
     const response = await fetch(convertedUrl, {
       redirect: "follow",
+      headers: {
+        "user-agent":
+          "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+      },
     });
-
     if (response.ok) {
+      const htmlRaw = await response.text();
+      const finalUrl = getElementFromHTML(htmlRaw, response.url);
       // The response.url property will contain the final redirected URL
-      id = id + 1;
-      const finalUrl = response.url;
-      res.json({ id: id, message: finalUrl, error: null }); // Respond with success
+      const newRes = await fetch(finalUrl, { redirect: "follow" });
+      res.json({ id: id, message: newRes.url, error: null }); // Respond with success
     } else {
       throw new Error("Request failed with status " + response.status);
     }
--- utils.js
+++ utils.js
@@ -1,12 +1,37 @@
+import cheerio from "cheerio";
+
 export function convertInstagramReelsToDdInstagram(originalUrl) {
+  return originalUrl.replace(
+    /https?:\/\/(www\.)?instagram\.com/g,
+    "https://www.ddinstagram.com"
+  );
+
   // Split the original URL into parts
-  const urlParts = originalUrl.split("/");
+  // const urlParts = originalUrl.split("/");
 
   // Get the unique code from the original URL
-  const code = urlParts[urlParts.length - 2];
+  // const code = urlParts[urlParts.length - 2];
 
   // Create the new URL in "ddinstagram" format
-  const newUrl = `https://www.ddinstagram.com/videos/${code}/1`;
+  // const newUrl = `https://www.ddinstagram.com/videos/${code}/1`;
 
-  return newUrl;
+  // return newUrl;
+}
+export function getElementFromHTML(html, url) {
+  const instagramPostPattern =
+    /^https:\/\/www\.ddinstagram\.com\/p\/[A-Za-z0-9_-]+\/?$/;
+  const instagramReelsPattern =
+    /^https:\/\/www\.ddinstagram\.com\/reels\/[A-Za-z0-9_-]+\/?$/;
+
+  const $ = cheerio.load(html);
+  if (instagramPostPattern.test(url)) {
+    const twitterImageMeta = $('meta[name="twitter:image"]');
+    const twitterImageContent = twitterImageMeta.attr("content");
+    return `https://www.ddinstagram.com${twitterImageContent}`;
+  }
+  if (instagramReelsPattern.test(url)) {
+    const twitterPlayerStreamMeta = $('meta[name="twitter:player:stream"]');
+    const twitterPlayerStreamContent = twitterPlayerStreamMeta.attr("content");
+    return `https://www.ddinstagram.com${twitterPlayerStreamContent}`;
+  }
 }
